a39ebd35d330affde3eee1e6176087cddcab0027
Utkarsh <utkarsh>
2016-06-04 14:31:17 +0530

    adding gem rails 12 factor

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
-gem 'devise_token_auth', github:"lynndylanhurley/devise_token_auth", branch: "master"
+gem 'devise_token_auth'#, github:"lynndylanhurley/devise_token_auth", branch: "master"
 gem 'omniauth' # required for devise_token_auth
 
+gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20160602132230) do
 
-  create_table "reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "reports", force: :cascade do |t|
     t.integer  "bgl"
     t.integer  "user_id"
     t.datetime "created_at", null: false
@@ -21,14 +24,14 @@ ActiveRecord::Schema.define(version: 20160602132230) do
     t.index ["user_id"], name: "index_reports_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "provider",                             default: "email", null: false
-    t.string   "uid",                                  default: "",      null: false
-    t.string   "encrypted_password",                   default: "",      null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        default: 0,       null: false
+    t.integer  "sign_in_count",          default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 20160602132230) do
     t.string   "nickname"
     t.string   "image"
     t.string   "email"
-    t.text     "tokens",                 limit: 65535
+    t.text     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["email"], name: "index_users_on_email", using: :btree
